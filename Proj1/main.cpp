daefcebcd942f9c7f73b276144e15975ff710170
krystalgamer <callofdutypsp@gmail.com>
2020-03-30 23:17:29 +0100

    Tabu search

--- Proj1/main.cpp
+++ Proj1/main.cpp
@@ -74,7 +74,9 @@ pair<SlideShow, SlideShow> loadInput(int input)
         "input/b_lovely_landscapes.txt",
         "input/c_memorable_moments.txt",
         "input/d_pet_pictures.txt",
-        "input/e_shiny_selfies.txt"};
+        "input/e_shiny_selfies.txt",
+        "input/g_sheisse.txt",
+	};
 
     file.open(files[input]);
     getline(file, line);
@@ -82,15 +84,17 @@ pair<SlideShow, SlideShow> loadInput(int input)
 	auto counter = 0;
 	while (getline(file, line))
     {
-        string orientation, _;
+        string orientation;
+		int	number_tags;
 
         Image *image = new Image();
         istringstream iss(line);
-        iss >> orientation >> _;
+        iss >> orientation;
+		iss >> number_tags;
 
         image->orientation = (orientation == "H" ? H :  V);
 
-        while (iss)
+        for (int i=0;i<number_tags;i++)
         {
             string tag;
             iss >> tag;
@@ -164,15 +168,29 @@ unsigned int evaluation(const SlideShow &slideshow){
         return points;
 }
 
+void print_slideshow(const SlideShow &slideshow){
+
+	cout << "START SLIDESHOW" << endl;
+	for(auto &element: slideshow){
+		cout << "ELEMENT" << endl;
+		for(auto &tag: element->get_tags())
+			cout << tag << endl;
+
+	}
+	cout << "END SLIDESHOW" << endl;
+
+}
 
 namespace Operator{
 
         SlideShow swap_slides(SlideShow &slideshow, int i, int j){
                 if(i == j)
                         return slideshow;
+
                 swap(slideshow[i], slideshow[j]);
                 slideshow[i]->index = i;
                 slideshow[j]->index = j;
+
                 return slideshow;
         }
 
@@ -190,17 +208,6 @@ namespace Operator{
 
 };
 
-void print_slideshow(const SlideShow &slideshow){
-
-	for(auto &element: slideshow){
-		cout << "ELEMENT" << endl;
-		for(auto &tag: element->get_tags())
-			cout << tag << endl;
-
-	}
-
-
-}
 
 int calculate_trasition(Slide* left, Slide* right) {
     auto left_tags = left->get_tags();
@@ -218,7 +225,7 @@ int calculate_trasition(Slide* left, Slide* right) {
 }
 
 int calc_around_slide(SlideShow& slideshow, int i) {
-    int sum{};
+    int sum = 0;
 
     if (i > 0) {
         sum += calculate_trasition(slideshow[i-1], slideshow[i]);
@@ -237,7 +244,7 @@ bool accept_move_hill_climb(int unused, int unused2, int delta){
 }
 
 inline bool accept_move_annealing(int iteration,int maxIteration , int delta){
-    if(delta >0 ){
+    if(delta >0){
         return true;
     }
     else if(delta == 0){return false;}
@@ -311,64 +318,118 @@ SlideShow climb_with_heuristic(pair<SlideShow, SlideShow> &slides, function<bool
 	return working_cpy;
 }
 
+long calculate_hash(const SlideShow &slideshow, int start_index){
+
+	long res = 0;
+	for(auto i = 0; i<16 && start_index+i<slideshow.size(); i++){
+		unsigned cur = 0;
+		cur = reinterpret_cast<long>(&slideshow[i+start_index]);
+
+		
+		cur += reinterpret_cast<long>(slideshow[i+start_index]->vert_images[0]);
+		if(slideshow[i+start_index]->is_vertical())
+			cur += reinterpret_cast<long>(slideshow[i+start_index]->vert_images[1]);
+
+		res += i*cur;
+	}
+
+	return res;
+}
+
+vector<long> block_hash(const SlideShow &slideshow){
+
+	vector<long> result;
+	result.reserve(slideshow.size()/16+1);
+	for(auto i=0;i<slideshow.size();i+=16){
+		result.push_back(calculate_hash(slideshow, i));
+	}
+	return result;
+}
+
 SlideShow tabu_search(pair<SlideShow, SlideShow> &slides){
     SlideShow working_cpy(slides.first);
 
-		std::random_device rd;
-		std::mt19937 g(rd());
+	std::random_device rd;
+	std::mt19937 g(rd());
 
-		shuffle(working_cpy.begin(), working_cpy.end(), g);
+	auto hashes = block_hash(working_cpy);
+	vector<pair<unsigned, vector<long>>> tabu_list;
 
-		std::uniform_int_distribution<> dis(0, working_cpy.size()-1);
-		std::uniform_int_distribution<> vert_dis(0, slides.second.size()-1);
+	std::uniform_int_distribution<> dis(0, working_cpy.size()-1);
+	std::uniform_int_distribution<> vert_dis(0, slides.second.size()-1);
 
-        size_t i = 0;
-        for_each(working_cpy.begin(), working_cpy.end(), [&i](auto s) {
-                    s->index=i++;
-                });
+	size_t i = 0;
+	for_each(working_cpy.begin(), working_cpy.end(), [&i](auto s) {
+				s->index=i++;
+			});
 
-		auto cur_value = evaluation(working_cpy);
-        cout << working_cpy.size() << "\n";
-		for(int i = 0; i<working_cpy.size(); i++){
-			auto l = dis(g);
-			auto r = dis(g);
-			if(l == r)
-				r = (r+1)%working_cpy.size();
+	cout << working_cpy.size() << "\n";
 
-            int before = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
-			Operator::swap_slides(working_cpy, l, r);
-            int after = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
-            int diff = after - before;
-			if(accept_move_annealing(i,working_cpy.size(),diff)){
-				cur_value += diff;
-				cout << "     SWAP - New Value! " << cur_value << " " << i << endl;
-				continue;
+	vector<function<SlideShow()>> operator_order;
+	operator_order.reserve(working_cpy.size());
+
+	auto cur_value = evaluation(working_cpy);
+	int best_index = -1;
+	for(int i = 0; i<working_cpy.size(); i++){
+
+		tuple<unsigned, function<SlideShow()>, int, int> cur_op = make_tuple(0, nullptr, 0, 0);
+		for(auto i=0;i<10;i++){
+
+			bool horizontal_swap = (slides.second.size() == 0 ? true : (random() % 2 == 0));
+
+			if(horizontal_swap){
+				auto l = dis(g);
+				auto r = dis(g);
+				if(l == r)
+					r = (r+1)%working_cpy.size();
+
+				auto operation = bind(Operator::swap_slides, ref(working_cpy), l, r);
+				int before = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
+				operation();
+				int after = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
+				operation();
+
+				if(get<0>(cur_op) < (after - before)+cur_value)
+					cur_op = make_tuple(cur_value + (after - before), operation, l, r);
 			}
-			Operator::swap_slides(working_cpy, l, r);
+			else{
+				auto vert_i = vert_dis(g);
+				auto vert_j = vert_dis(g);
 
-			if(slides.second.size() == 0)
-				continue;
+				auto l = slides.second[vert_i]->index;
+				auto r = slides.second[vert_j]->index;
 
+				auto operation = bind(Operator::swap_verticals, ref(slides.second), vert_i, vert_j);
+				auto before = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
+				operation();
+				auto after = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
+				operation();
 
-            auto vert_i = vert_dis(g);
-            auto vert_j = vert_dis(g);
-
-            l = slides.second[vert_i]->index;
-            r = slides.second[vert_j]->index;
-
-            before = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
-            Operator::swap_verticals(slides.second, vert_i, vert_j);
-            after = calc_around_slide(working_cpy, l) + calc_around_slide(working_cpy, r);
-            diff = after - before;
-			if(accept_move_annealing(i,working_cpy.size(),diff)){
-				cur_value += diff;
-				cout << "VERT SWAP - New Value! " << cur_value << " " << i << endl;
-				continue;
+				if(get<0>(cur_op) < (after - before)+cur_value)
+					cur_op = make_tuple(cur_value + after - before, operation, l, r);
 			}
-            Operator::swap_verticals(slides.second, vert_i, vert_j);
+
 		}
 
-        return working_cpy;
+		if(get<1>(cur_op) == nullptr)
+			continue;
+
+		operator_order.push_back(get<1>(cur_op));
+		operator_order[operator_order.size()-1]();
+		if(cur_value < get<0>(cur_op)){
+			best_index = operator_order.size()-1;
+			cur_value = get<0>(cur_op);
+			cout << "Found better one " << cur_value << " " << i << endl;
+		}
+	}
+
+	for(auto it=operator_order.rbegin(); it!=operator_order.rend(); it++)
+		(*it)();
+
+	for(auto i=0;i<=best_index;i++)
+		operator_order[i]();
+
+	return working_cpy;
 }
 
 
@@ -376,12 +437,15 @@ int main(){
 
 		srand(time(nullptr));
         for(int i=1; i<2; i++){
-                auto before = loadInput(i);
+			auto before = loadInput(i);
+			cout << evaluation(tabu_search(before)) << endl;
+			/*
                 cout << "Before: " << evaluation(before.first) << endl;
                 auto after = climb_with_heuristic(before, accept_move_annealing);
                 cout << "After Annealing: " << evaluation(after) << endl;
                 after = climb_with_heuristic(before, accept_move_hill_climb);
                 cout << "After Hill Climb: " << evaluation(after) << endl;
+				*/
         }
         return 0;
 }
